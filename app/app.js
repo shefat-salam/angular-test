ea36781b3a1768183d076e430e52a855fae000fb
shefat salam <mir_salammia@yahoo.com>
2018-03-17 17:07:29 +0600

    modified fiels

--- app/app.js
+++ app/app.js
@@ -1,14 +1,19 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  
+var app = angular.module('myApp', [
+  'ngRoute'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
-
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider
+    .when('/view1', {
+      templateUrl: 'view1/views/view1.html',
+      controller: 'controllers/View1Ctrl'
+    })
+    .when('/view2', {
+      templateUrl: 'view2/views/view2.html',
+      controller: 'controllers/View2Ctrl'
+    })
+    .otherwise({redirectTo: '/view1'});
 }]);
---
+++ app/view1/controllers/view1.js
@@ -0,0 +1,13 @@
+'use strict';
+
+
+// .config(['$routeProvider', function($routeProvider) {
+//   $routeProvider.when('/view1', {
+//     templateUrl: 'view1/view1.html',
+//     controller: 'View1Ctrl'
+//   });
+// }])
+
+app.controller('View1Ctrl', [function() {
+
+}]);

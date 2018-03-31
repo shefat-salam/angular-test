43d2c0d28504c218d905cfc56502dcd81d07b4e2
shefat salam <mir_salammia@yahoo.com>
2018-03-31 11:51:52 +0600

    working with directives

--- app/directives/InboxDrctv.js
+++ app/directives/InboxDrctv.js
@@ -6,26 +6,32 @@
       scope: false,
       templateUrl: '../directives/inbox.tmpl.html',
       controllerAs: 'inbox',
-      controller: function (InboxFactory) {
+      controller: function (InboxFactory,$scope,$filter) {
         this.messages = [];
-
-     
         this.goToMessage = function (id) {
           InboxFactory.goToMessage(id);
         };
-        
-        // this.deleteMessage = function (messages) {
-        //   InboxFactory.deleteMessage(messages);
-        // };
-       
+      
         InboxFactory.getMessages()
-          .then( angular.bind( this, function then() {
-              this.messages = InboxFactory.messages;
-            }) 
-          );    
+          .then( angular.bind( this, function() {this.messages = InboxFactory.messages;}) );  
+           
+          $scope.$watch("messages", function(n, o) {
+            var checked = $filter("filter")(n, { checked: true });
+            debugger;
+            console.log(checked);
+            if (checked.length) {
+                scope.selected = checked;
+                $('.deleteBtn').removeAttr('disabled');
+            } else {
+                $('.deleteBtn').attr('disabled', 'disabled');
+            }
+        }, true);  
 
       },
       link: function (scope, element, attrs, ctrl) {
+        
+     
+  
         scope.isAll = false;
         scope.selectAll = function(messages) {
          
@@ -41,6 +47,7 @@
                 scope.isAll = false;
             }
         };
+        
         scope.deleteMessage = function (messages) {
             angular.forEach(messages, function(row, index) {
                 if ( messages[index].checked) {
